4710db0cf906eefadd284180ecf4ae42ec506be3
AChin1311 <daphne011552000@yahoo.com.tw>
2018-05-17 21:58:22 -0700

    load data from pickles

--- usage_token.py
+++ usage_token.py
@@ -2,31 +2,29 @@ import tensorflow as tf
 import os
 from bilm import TokenBatcher, BidirectionalLanguageModel, weight_layers, \
     dump_token_embeddings
-import pandas as pd
-import re
 import numpy as np
+import pickle
 
-
-def make_set(df):
+def make_set(data):
     all_words = []
-    for _, row in df.iterrows():
-        row['title'].replace("\'", "").replace("\"", "")
-        row['content'].replace("\'", "").replace("\"", "")
-        all_words.extend(re.findall(r'\w+|\S+', row['title']))
-        all_words.extend(re.findall(r'\w+|\S+', row['content']))
+    for line in data:
+        print(line)
+        all_words.extend(' '.join(line).split())
     all_tokens = set(['<S>', '</S>'] + all_words)
     return all_tokens
 
-def load_data():
-    ALL_NEWS_DIR = 'all-the-news'
-    df = pd.read_csv(ALL_NEWS_DIR+'/articles1.csv')
-    # df = df.append(pd.read_csv(ALL_NEWS_DIR+'/articles2.csv'))
-    # df = df.append(pd.read_csv(ALL_NEWS_DIR+'/articles3.csv'))
-    df2 = df[['title','content']]
-    return df2    
+def pickle2tuple(nr):
+        [heads, desc, _] = pickle.load(open('pickles/all-the-news_'+nr+'.pickle', 'rb'))
+        print(len(heads))
+        print(len(desc))
+        print(type(heads), type(desc))
+        return heads + desc
 
-data = load_data()
+data = pickle2tuple('5000')
+print(len(data))
 all_tokens = make_set(data)
+print(len(all_tokens))
+
 vocab_file = 'vocab_all_news.txt'
 with open(vocab_file, 'w') as fout:
     fout.write('\n'.join(all_tokens))
